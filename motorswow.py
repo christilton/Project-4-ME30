261d3ae4ce0d35541280d35ec80322a35b37ed5f
christilton <cjt516@gmail.com>
2022-12-04 15:13:19 -0500

    Update motorswow.py

--- motorswow.py
+++ motorswow.py
@@ -5,6 +5,8 @@ control_pins = [32,36,38,40]
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
+
+pos = 0
 halfstep_seq = [
   [1,0,0,0],
   [1,1,0,0],
@@ -20,4 +22,6 @@ for i in range(512):
     for pin in range(4):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
+    pos += .5
+    print(pos)
 GPIO.cleanup()
