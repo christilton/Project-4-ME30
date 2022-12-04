0b70094be3218390d91a741bd2dd4ce8fc0de30a
christilton <cjt516@gmail.com>
2022-12-04 14:52:52 -0500

    Update motorswow.py

--- motorswow.py
+++ motorswow.py
@@ -1,8 +1,23 @@
-import RPistepper as stp
-M1_pins = [32, 36, 38, 40]
-with stp.Motor(M1_pins) as M1:
-    for i in range(10):               # moves 20 steps,release and wait
-        print M1
-        M1.move(20)
-        M1.release()
-        raw_input('enter to execute next step')
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+control_pins = [32,36,38,40]
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+for i in range(512):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    time.sleep(0.001)
+GPIO.cleanup()
