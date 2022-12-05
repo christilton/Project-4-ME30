fd51c1c87421eb21f51bd26688e389a89fcc15a1
christilton <cjt516@gmail.com>
2022-12-05 13:54:17 -0500

    Update getinstructions.py

--- getinstructions.py
+++ getinstructions.py
@@ -53,19 +53,19 @@ while True:
         if (type == "speed" and float(value) == -1): #reverse
             GPIO.output(13,GPIO.HIGH)
             GPIO.output(15,GPIO.LOW)
-        elif (type == "speed" and float(value) == 0): #stop
+        if (type == "speed" and float(value) == 0): #stop
             GPIO.output(13,GPIO.LOW)
             GPIO.output(15,GPIO.LOW)
-        elif (type == 'speed' and float(value) > 0): #forward
+        if (type == 'speed' and float(value) > 0): #forward
             GPIO.output(15, GPIO.HIGH)
             GPIO.output(13, GPIO.LOW)
-        elif (type == "direction" and value == "left"):
+        if (type == "direction" and value == "left"):
             STATE = LEFTTURN
-        elif (type == "direction" and value == "right"):
+        if (type == "direction" and value == "right"):
             STATE = RIGHTTURN
-        elif (type == "direction" and value == "none"):
+        if (type == "direction" and value == "none"):
             STATE = STOPPED
-        elif (steps == 0 or steps == 180):
+        if (steps == 0 or steps == 180):
             STATE = STOPPED
         if (STATE == RIGHTTURN and steps < 180):
             for halfstep in range(8):
@@ -73,13 +73,13 @@ while True:
                   GPIO.output(control_pins_b[pin], halfstep_seq[halfstep][pin])
                   time.sleep(.0001)
             steps += 1
-        elif (STATE == LEFTTURN and steps > 0):
+        if (STATE == LEFTTURN and steps > 0):
             for halfstep in range(8):
               for pin in range(4):
                   GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
                   time.sleep(.0001)
             steps -= 1
-        elif(STATE == STOPPED):
+        if(STATE == STOPPED):
             pass
         else:
             pass
