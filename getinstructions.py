bae5c45e6c9856e2e7eb1fb5381637e7359d817e
christilton <cjt516@gmail.com>
2022-12-14 13:09:39 -0500

    Update getinstructions.py

--- getinstructions.py
+++ getinstructions.py
@@ -70,17 +70,11 @@ def handle_movement(instructions,STATE,control_pins,control_pins_b,halfstep_seq,
           for pin in range(4):
               GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
               time.sleep(.0007)
-        steps += 1
-        print(steps)
-        return steps
     if (STATE == LEFTTURN):
         for halfstep in range(8):
           for pin in range(4):
               GPIO.output(control_pins_b[pin], halfstep_seq[halfstep][pin])
               time.sleep(.0007)
-        steps -= 1
-        print(steps)
-        return steps
     if(STATE == STOPPED):
         pass
 
